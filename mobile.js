2c0d8bc99802ef640a6629b7dced54fd5b876ba4
Tamires DS <tamires.castro5@aluno.ce.gov.br>
2023-05-19 09:03:16 -0300

    first commit

---
+++ mobile.js
@@ -0,0 +1,18 @@
+
+function ativarNome(){
+    let nome = 'Tamires';
+    console.log(nome)
+    
+    document.getElementById('aparecer').innerHTML = nome
+}
+
+
+function somarNumeros(){
+    let a = 5;
+    let b = 10;
+    let c = a*b;
+
+    document.getElementById('aparecer').innerHTML = c
+}
+
+    
